0a6c47e7371ad5e2f52722be44088a00f1af4e68
landmanbester <lbester@ska.ac.za>
2023-01-04 15:48:03 +0200

    update casa cabs

--- casa/casacabmaker.py
+++ casa/casacabmaker.py
@@ -22,59 +22,97 @@ def get_type(a):
 if __name__=="__main__":
     taskname = sys.argv[1]
 
-    cmd = f'help({taskname}); import numpy as np; np.savez({taskname}.npz, **{taskname}.parameters) quit()'
+    # preferable to build the cab from the docstring but they don't all adhere
+    # to a consistent format (note thi smay produce garbage)
+    try:
+        # import numpy as np; np.savez({taskname}.npz, **{taskname}.parameters)
+        cmd = f'help({taskname}); quit()'
 
-    with open(taskname + '.txt', 'w') as output:
-        subprocess.Popen(['casa', '--nologger', '--log2term', '--nologfile', '-c', cmd],
-                         stdout=output)
+        with open(taskname + '.txt', 'w') as output:
+            subprocess.Popen(['casa', '--nologger', '--log2term', '--nologfile', '-c', cmd],
+                            stdout=output)
 
-    # need to wait for the above to finish
-    time.sleep(10)
+        # need to wait for the above to finish
+        time.sleep(10)
 
-    quit()
+        f = open(taskname + '.txt', 'r')
+        # find just the relevant help string in the file
+        fulltxt = f.readlines()
+        for i, l in enumerate(fulltxt):
+            if l.find('Arguments') > 0:
+                l0 = i+1
+            elif l.find('Returns') > 0:
+                lf = i
+                break
 
-    f = open(taskname + '.txt', 'r')
-    # find just the relevant help string in the file
-    fulltxt = f.readlines()
-    for i, l in enumerate(fulltxt):
-        if l.find('Arguments') > 0:
-            l0 = i+1
-        elif l.find('Returns') > 0:
-            lf = i
-            break
-
-    docs = fulltxt[l0:lf]
-    x = {}
-    for i, l in enumerate(docs):
-        l = l.strip()[1:].strip()
-        if len(l):
-            # print(i, l)
-            idx = l.find(':')
-            if idx > -1:  # must be a keyword
-                l0 = l[0:idx].strip()
-                l1 = l[idx:].strip()[1:].strip()
-                if 'Default Value' in l0:
+        docs = fulltxt[l0:lf]
+        x = {}
+        for i, l in enumerate(docs):
+            l = l.strip()[1:].strip()
+            if len(l):
+                # print(i, l)
+                idx = l.find(':')
+                if idx > -1:  # must be a keyword
+                    l0 = l[0:idx].strip()
+                    l1 = l[idx:].strip()[1:].strip()
+                    if 'Default Value' in l0:
+                        try:
+                            val = eval(l1)
+                            x[key]['default'] = val
+                            x[key]['dtype'] = get_type(val)
+                        except:
+                            x[key]['default'] = l1
+                            x[key]['dtype'] = 'str'
+                    elif 'Allowed Values' in l0:
+                        x[key]['choices'] = []
+                    else:  # must be a key
+                        key = l0
+                        x.setdefault(key, {})
+                        x[key]['info'] = l1.strip()
+                else:
                     try:
-                        val = eval(l1)
-                        x[key]['default'] = val
-                        x[key]['dtype'] = get_type(val)
+                        x[key]['choices'].append(l)
                     except:
-                        x[key]['default'] = l1
-                        x[key]['dtype'] = 'str'
-                elif 'Allowed Values' in l0:
-                    x[key]['choices'] = []
-                else:  # must be a key
-                    key = l0
-                    x.setdefault(key, {})
-                    x[key]['info'] = l1.strip()
+                        print(f'Check on failure at {key}')
+    except:
+        cmd = f'inp({taskname}); quit()'
+
+        with open(taskname + '.txt', 'w') as output:
+            subprocess.Popen(['casa', '--nologger', '--log2term', '--nologfile', '-c', cmd],
+                            stdout=output)
+
+        # need to wait for the above to finish
+        time.sleep(10)
+
+        f = open(taskname + '.txt', 'r')
+        x = {}
+        for i, l in enumerate(f.readlines()):
+            l = l.strip()
+            print(i, l)
+            if len(l) == 0:
+                continue
+            elif not l.startswith('#'):
+                ll = l.split('=')
+                if len(ll) == 2:
+                    l0, l1 = l.split('=')
+                else:
+                    l0 = ll[0]
+                    l1 = ''.join(ll[1:])
+                l1, l2 = l1.split('#')
+                key = l0.strip()
+                val = eval(l1.strip())
+                docs = l2.strip()
+                print(i, key, val, docs)
+                x.setdefault(key, {})
+                x[key]['default'] = val
+                x[key]['info'] = docs
+                x[key]['dtype'] = get_type(val)
             else:
-                try:
-                    x[key]['choices'].append(l)
-                except:
-                    print(f'Check on failure at {key}')
+                docs = l[1:].strip()
+                x[key]['info'] += ' ' + docs
 
     f.close()
-    # os.remove(taskname + '.txt')
+    os.remove(taskname + '.txt')
 
     if 'vis' in x.keys():
         x['vis']['required'] = True
